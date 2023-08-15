9b54761d0d4e200d1539eab9a2d25da34e7acfb7
sadiejaneb <sadiejaneb@gmail.com>
2023-08-15 17:41:25 -0400

    Django and allauth
    installed Django app, 'feed'', and created a Post model with text and date fields.
    
    added a HomePage view with a ListView of the posts from 'feed'
    
    configured url's to include feed in ""
    
    created a base.html in the app and added settings for django allauth in setings.py
    
    added login/logout /home links to base.html to appear on all allauth

---
+++ feed/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Post (models.Model):
+    text = models.CharField(max_length=140, blank=False, null=False)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text[0:100]
